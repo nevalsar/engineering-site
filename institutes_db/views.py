3a0990da18ebcff0874507558421d7614da1b537
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2015-04-09 01:25:22 +0530

    Display table from database
    - Display contents of College table from database
    - Paginate and add search function using jQuery plugin
    
    Signed-off-by: Nevin Valsaraj <nevin.valsaraj32@gmail.com>

--- institutes_db/views.py
+++ institutes_db/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
+from institutes_db.models import Address, Approves, Board, City, College, Contact, Course, Degree, Department, Founding_History, Located_at, Offer_Statistics, Offers, Qualifying_Examination, State, Web_Links
+
 def index(request):
     return render(request, 'institutes_db/index.html')
 
 def table_as_user(request, profile_name):
     if profile_name in ["student", "faculty", "college", "recruiter", "analyst"]:
+        colleges = College.objects.all()
         return render(request, 'institutes_db/table.html', {
             'profile_name': profile_name,
+            'colleges': colleges,
             })
     else:
         return render(request, 'institutes_db/table.html')
