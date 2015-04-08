45dd1c9707f06c126104271bdd81ad1132ce1282
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2015-04-08 13:42:11 +0530

    Move data files to separate folder
    Modify populatedb.py accordingly (needs to be in project root)
    
    Signed-off-by: Nevin Valsaraj <nevin.valsaraj32@gmail.com>

--- populatedb.py
+++ populatedb.py
@@ -8,25 +8,25 @@ from institutes_db.models import Address, Approves, Board, City, College, Contac
 
 # Don't change the stuff above
 
-with open('dbms-state.csv', 'rb') as csvfile:
+with open('data/dbms-state.csv', 'rb') as csvfile:
 	datareader = csv.reader(csvfile, delimiter=',')
 	for row in datareader:
 		temp = State(state_name=row[0].strip())
 		temp.save()
 
-with open('dbms-city.csv', 'rb') as csvfile:
+with open('data/dbms-city.csv', 'rb') as csvfile:
 	datareader = csv.reader(csvfile, delimiter=',')
 	for row in datareader:
 		temp = City(city_name=row[0].strip())
 		temp.save()
 
-with open('dbms-board.csv', 'rb') as csvfile:
+with open('data/dbms-board.csv', 'rb') as csvfile:
 	datareader = csv.reader(csvfile, delimiter=',')
 	for row in datareader:
 		temp = Board(board_name=row[0].strip())
 		temp.save()
 
-with open('dbms-data1.csv', 'rb') as csvfile:
+with open('data/dbms-data1.csv', 'rb') as csvfile:
 	datareader = csv.reader(csvfile, delimiter=',')
 	for row in datareader:
 		#print row[0].strip()
@@ -35,7 +35,7 @@ with open('dbms-data1.csv', 'rb') as csvfile:
 
 
 
-with open('dbms-course_name.csv', 'rb') as csvfile:
+with open('data/dbms-course_name.csv', 'rb') as csvfile:
 	datareader = csv.reader(csvfile, delimiter=',')
 	for row in datareader:
 		temp = Course(course_name = row[0].strip())
@@ -45,34 +45,14 @@ with open('dbms-course_name.csv', 'rb') as csvfile:
 
 
 
-with open('dbms-exam.csv', 'rb') as csvfile:
+with open('data/dbms-exam.csv', 'rb') as csvfile:
 	datareader = csv.reader(csvfile, delimiter=',')
 	for row in datareader:
 		temp = Qualifying_Examination( exam_name = row[0].strip())
 		temp.save()
 
-with open('dbms-degree.csv', 'rb') as csvfile:
+with open('data/dbms-degree.csv', 'rb') as csvfile:
 	datareader = csv.reader(csvfile, delimiter=',')
 	for row in datareader:
 		temp = Degree( degree_name = row[0].strip())
 		temp.save()
-
-
-
-
-		
-
-
-#temp=College(college_nam
-# Sample object creation syntax
-
-
-# If a table has foreign keys, use the following syntax
-
-# print College.objects.all()
-
-# If a table has foreign keys, use the following syntax
-# temp = Approves()
-# temp.college = College.objects.get(college_name="IIT")
-# temp.board = Board.objects.get(board_name="CBSE")
-# temp.save()
